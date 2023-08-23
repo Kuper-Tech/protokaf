74dc1064e5160231357d09d322d84b7beafe918f
Vadim Inshakov <vadiminshakov@gmail.com>
2023-08-24 02:39:03 +0500

    add -offsets (-o) flag

--- cmd/cmd_consume.go
+++ cmd/cmd_consume.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"context"
 	"errors"
-
+	"fmt"
 	"github.com/SberMarket-Tech/protokaf/internal/kafka"
 	"github.com/SberMarket-Tech/protokaf/internal/utils/dump"
 	"github.com/Shopify/sarama"
@@ -11,6 +11,9 @@ import (
 	"github.com/jhump/protoreflect/dynamic"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"strconv"
+	"strings"
+	"sync"
 )
 
 func NewConsumeCmd() *cobra.Command {
@@ -19,6 +22,7 @@ func NewConsumeCmd() *cobra.Command {
 		topicsFlag []string
 		countFlag  int
 		noCommit   bool
+		offsets    []string
 	)
 
 	cmd := &cobra.Command{
@@ -41,6 +45,7 @@ func NewConsumeCmd() *cobra.Command {
 			if noCommit {
 				kafkaConfig.Consumer.Offsets.AutoCommit.Enable = false
 			}
+
 			// consumer
 			consumer, err := kafka.NewConsumerGroup(viper.GetStringSlice("broker"), groupFlag, kafkaConfig)
 			if err != nil {
@@ -58,6 +63,19 @@ func NewConsumeCmd() *cobra.Command {
 				handler := &protoHandler{
 					MaxCount: countFlag,
 					desc:     md,
+
+					topics:    topicsFlag,
+					partition: int32(0),
+				}
+
+				// set offset
+				if offsets != nil {
+					offsetsArg, err := parseOffsetsFlag(offsets)
+					if err != nil {
+						log.Errorf("Failed to parse offset: %s", err)
+						return
+					}
+					handler.offsets = offsetsArg
 				}
 
 				err := consumer.Consume(context.Background(), topicsFlag, handler)
@@ -91,6 +109,8 @@ func NewConsumeCmd() *cobra.Command {
 	flags.StringSliceVarP(&topicsFlag, "topic", "t", []string{}, "Topic to consume from")
 	flags.IntVarP(&countFlag, "count", "c", 0, "Exit after consuming this number of messages")
 	flags.BoolVar(&noCommit, "no-commit", false, "Consume messages without commiting offset")
+	flags.StringSliceVarP(&offsets, "offsets", "o", nil, "Start consuming from this offsets: "+
+		"topic.name1:123,topic.name2:321 (offsets for every topic) or 123 (one global offset for all topics), default: newest)")
 
 	_ = cmd.MarkFlagRequired("group")
 	_ = cmd.MarkFlagRequired("topic")
@@ -98,12 +118,71 @@ func NewConsumeCmd() *cobra.Command {
 	return cmd
 }
 
+const globalOffset = "global"
+
+func parseOffsetsFlag(offsetsFlag []string) (offsets map[string]int64, err error) {
+	offsets = make(map[string]int64, len(offsetsFlag))
+	for _, offset := range offsetsFlag {
+		i, err := strconv.ParseInt(offset, 10, 64)
+		if err == nil {
+			return map[string]int64{globalOffset: i}, nil
+		}
+
+		topicOffsetPair := strings.Split(offset, ":")
+		if len(topicOffsetPair) != 2 {
+			return nil, fmt.Errorf("invalid offset format: %s, "+
+				"expected: topic:123 (topic name and offset value) or 123 (global offset value for all topics)", offset)
+		}
+		offsetForTopic, err := strconv.ParseInt(topicOffsetPair[1], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid offset format: %s, "+
+				"failed to parse offset value: %s\n"+
+				"expected: topic:123 (topic name and offset value) or 123 (global offset value for all topics)\n"+
+				"error: %s", topicOffsetPair[1], offset, err)
+		}
+		offsets[topicOffsetPair[0]] = offsetForTopic
+	}
+
+	return offsets, nil
+}
+
 type protoHandler struct {
 	desc              *desc.MessageDescriptor
 	MaxCount, counter int
+	topics            []string
+	partition         int32
+	offsets           map[string]int64
 }
 
-func (protoHandler) Setup(_ sarama.ConsumerGroupSession) error   { return nil }
+var once sync.Once
+
+func (p protoHandler) Setup(sess sarama.ConsumerGroupSession) error {
+	once.Do(func() {
+		goffst, isGlobalOffsetSet := p.offsets[globalOffset]
+		for _, topic := range p.topics {
+			if partFromFlags := flags.Partition; partFromFlags > 0 {
+				p.partition = partFromFlags
+			}
+
+			if isGlobalOffsetSet {
+				sess.ResetOffset(topic, p.partition, goffst, "")
+				continue
+			}
+
+			if p.offsets != nil {
+				offset, ok := p.offsets[topic]
+				if !ok {
+					println("ok2")
+					continue
+				}
+				println("ok3")
+				sess.ResetOffset(topic, p.partition, offset, "")
+			}
+		}
+	})
+	return nil
+}
+
 func (protoHandler) Cleanup(_ sarama.ConsumerGroupSession) error { return nil }
 
 // ErrMaximumReached error if limit reached
